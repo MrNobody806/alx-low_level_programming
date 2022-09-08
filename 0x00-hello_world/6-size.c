760d7e66d40df58dd5fdebe07d3d635df518732a
Manuhe Wolde <110993818+MrNobody806@users.noreply.github.com>
2022-09-09 01:54:16 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-
 /**
-main - print the string in the put function
-Description: using the main function
-this program prints "with proper grammar, but the outcome is a piece of art"
-Return: 0
+* main - print the string in the put function
+* Description: using the main function
+* this program prints "with proper grammar, but the outcome is a piece of art"
+* Return: 0
 */
 int main(void)
-
 {
 char c;
 int i;
 long int;
 long long intt;
 float f;
-
-
-          printf ("Size of a char : % ld byte (s)\n", sizeof (c));
-          printf ("Size  of an int : % ld byte (s)\n", sizeof (i));
-          printf ("Size of a long int : % ld byte (s)\n", sizeof (int));
-          printf ("Size of a long long int : % ld byte (s)\n", sizeof (intt));
-          printf ("Size of a float : % ld byte (s)\n", sizeof (f));
- return (0);
+printf ("Size of a char : % ld byte (s)\n", sizeof (c));
+printf ("Size  of an int : % ld byte (s)\n", sizeof (i));
+printf ("Size of a long int : % ld byte (s)\n", sizeof (int));
+printf ("Size of a long long int : % ld byte (s)\n", sizeof (intt));
+printf ("Size of a float : % ld byte (s)\n", sizeof (f));
+return (0);
 }
