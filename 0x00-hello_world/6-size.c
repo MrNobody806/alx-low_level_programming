8fda25aaa0b1c9b193775fb8c3de3d87c0754952
Manuhe Wolde <110993818+MrNobody806@users.noreply.github.com>
2022-09-09 02:31:59 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+
 /**
-* main - print the string in the put function
-* Description: using the main function
-* this program prints "with proper grammar, but the outcome is a piece of art"
-* Return: 0
-*/
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
 int main(void)
 {
-char c;
-int i;
-long int;
-long long intt;
-float f;
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of an int: %ld byte(s)\n", sizeof(i));
-printf("Size of a long int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(intt));
-printf("Size of a float: %ld byte(s)\n", sizeof(f));
-return (0);
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longSize = sizeof(long int);
+	int longlongSize = sizeof(long long int);
+	int floatSize = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", charSize);
+	printf("Size of an int: %d byte(s)\n", intSize);
+	printf("Size of a long int: %d byte(s)\n", longSize);
+	printf("Size of a long long int: %d byte(s)\n", longlongSize);
+	printf("Size of a float: %d byte(s)\n", floatSize);
+	return (0);
 }
