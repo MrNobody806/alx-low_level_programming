4125939e407f0592786784405cd7590367753782
Manuhe Wolde <110993818+MrNobody806@users.noreply.github.com>
2022-09-09 02:08:51 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int i;
 long int;
 long long intt;
 float f;
-printf ( "Size of a char: % ld byte(s)\n", sizeof(c) );
-printf ( "Size of an int: % ld byte(s)\n", sizeof(i) );
-printf ( "Size of a long int: % ld byte(s)\n", sizeof(int) );
-printf ( "Size of a long long int: % ld byte(s)\n", sizeof(intt) );
-printf ( "Size of a float: % ld byte(s)\n", sizeof(f) );
+printf("Size of a char: % ld byte(s)\n", sizeof(c));
+printf("Size of an int: % ld byte(s)\n", sizeof(i));
+printf("Size of a long int: % ld byte(s)\n", sizeof(int));
+printf("Size of a long long int: % ld byte(s)\n", sizeof(intt));
+printf("Size of a float: % ld byte(s)\n", sizeof(f));
 return (0);
 }
